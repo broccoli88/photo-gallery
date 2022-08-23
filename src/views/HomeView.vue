5e9d12328732d18b0a6ce8fc2443c6ebdeb760f2
broccoli88 <kiai@Deus-Ex-Machina.local>
2022-08-23 20:45:47 +0200

    Photo Gallery - begining

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="starting-page">
+        <h1>Photo Gallery</h1>
+        <h2>Photo Gallery</h2>
+        <h3>Photo Gallery</h3>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+            voluptate atque sit! Ea blanditiis nesciunt voluptate reiciendis
+            quaerat sequi, officia sit aspernatur delectus? Recusandae, totam
+            nobis! Omnis tenetur ipsum molestias.
+        </p>
+    </div>
+</template>
+<script setup></script>
+<style scoped></style>
